32066d862b98676628fa713a282d22ea7b2da2cf
Monika <monisiaz2103@gmail.com>
2021-02-21 21:36:40 +0100

    Changed Form from class to functional component with hooks

--- front/src/components/Form/Form.js
+++ front/src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button, Col } from "react-bootstrap";
 import { connect } from "react-redux";
 import style from "./Form.module.css";
@@ -8,43 +8,38 @@ import EndOfOrder from "./EndOfOrder";
 
 const POST_URL = "http://localhost:3001/api/order";
 
-const initialState = {
-  order: [],
+const initialFormData = {
   first_name: "",
   last_name: "",
   city: "",
   zip_code: "",
-  validation: true,
-  isOrder: false,
 };
 
-class Formular extends React.Component {
-  state = initialState;
+const Formular = ({ booksInCart, clearCart }) => {
+  const [formData, setFormData] = useState(initialFormData);
+  const [validation, setValidation] = useState(true);
+  const [isOrder, setIsOrder] = useState(false);
+  const [order, setOrder] = useState([]);
 
-  prepareBooksInCartToPost = (booksInCart) => {
+  const prepareBooksInCartToPost = (booksInCart) => {
     const idsArray = Object.keys(booksInCart);
     const orderToPost = idsArray.map((id) => {
       return { id: Number(id), quantity: booksInCart[id].quantity };
     });
-    this.setState({
-      order: orderToPost,
-    });
+    setOrder(orderToPost);
   };
 
-  handleSubmit = () => {
-    let validationElements = checkValidation(this.state);
+  const handleSubmit = () => {
+    let validationElements = checkValidation(formData);
     let valueValidationElementsArray = Object.values(validationElements).filter(
       (element) => element === false
     );
     if (valueValidationElementsArray.length > 0) {
-      this.setState({ ...this.state, validation: false });
+      setValidation(false);
     } else {
       const dataToPost = {
-        order: this.state.order,
-        first_name: this.state.first_name,
-        last_name: this.state.last_name,
-        city: this.state.city,
-        zip_code: this.state.zip_code,
+        order: order,
+        ...formData,
       };
       fetch(POST_URL, {
         method: "POST",
@@ -52,8 +47,11 @@ class Formular extends React.Component {
         body: JSON.stringify(dataToPost),
       })
         .then(() => {
-          this.props.clearCart();
-          this.setState({...initialState, isOrder:true});
+          clearCart();
+          setFormData(initialFormData);
+          setValidation(true);
+          setOrder([]);
+          setIsOrder(true);
         })
         .catch((err) => {
           console.log(err);
@@ -61,104 +59,96 @@ class Formular extends React.Component {
     }
   };
 
-  handleOnChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
+  const handleOnChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
-  checkElementValidation = (element) => {
-    let validationElements = checkValidation(this.state);
+  const checkElementValidation = (element) => {
+    let validationElements = checkValidation(formData);
     return validationElements[element];
   };
 
-  componentDidMount() {
-    this.prepareBooksInCartToPost(this.props.booksInCart);
-  }
+  useEffect(() => {
+    prepareBooksInCartToPost(booksInCart);
+  }, []);
 
-  render() {
-    return (
-      !this.state.isOrder ?
-      <div className={style.formularBox}>
-        <Form>
-          <Form.Group>
+  return !isOrder ? (
+    <div className={style.formularBox}>
+      <Form>
+        <Form.Group>
+          <Form.Control
+            isInvalid={
+              validation
+                ? false
+                : !checkElementValidation("firstNameValidation")
+            }
+            placeholder="Imię"
+            name="first_name"
+            value={formData.first_name}
+            onChange={handleOnChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Wymagane 4 - 50 znaków
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Control
+            isInvalid={
+              validation ? false : !checkElementValidation("lastNameValidation")
+            }
+            placeholder="Nazwisko"
+            name="last_name"
+            value={formData.last_name}
+            onChange={handleOnChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Wymagane 5 - 50 znaków
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Row>
+          <Form.Group as={Col}>
             <Form.Control
               isInvalid={
-                this.state.validation
-                  ? false
-                  : !this.checkElementValidation("firstNameValidation")
+                validation ? false : !checkElementValidation("cityValidation")
               }
-              placeholder="Imię"
-              name="first_name"
-              value={this.state.first_name}
-              onChange={this.handleOnChange}
+              placeholder="Miejscowość"
+              name="city"
+              value={formData.city}
+              onChange={handleOnChange}
             />
             <Form.Control.Feedback type="invalid">
-              Wymagane 4 - 50 znaków
+              Pole wymagane
             </Form.Control.Feedback>
           </Form.Group>
 
-          <Form.Group>
+          <Form.Group as={Col}>
             <Form.Control
               isInvalid={
-                this.state.validation
+                validation
                   ? false
-                  : !this.checkElementValidation("lastNameValidation")
+                  : !checkElementValidation("zipCodeValidation")
               }
-              placeholder="Nazwisko"
-              name="last_name"
-              value={this.state.last_name}
-              onChange={this.handleOnChange}
+              placeholder="Kod pocztowy"
+              name="zip_code"
+              value={formData.zip_code}
+              onChange={handleOnChange}
             />
             <Form.Control.Feedback type="invalid">
-              Wymagane 5 - 50 znaków
+              Wymagany format _ _ - _ _ _
             </Form.Control.Feedback>
           </Form.Group>
-          <Form.Row>
-            <Form.Group as={Col}>
-              <Form.Control
-                isInvalid={
-                  this.state.validation
-                    ? false
-                    : !this.checkElementValidation("cityValidation")
-                }
-                placeholder="Miejscowość"
-                name="city"
-                value={this.state.city}
-                onChange={this.handleOnChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                Pole wymagane
-              </Form.Control.Feedback>
-            </Form.Group>
+        </Form.Row>
 
-            <Form.Group as={Col}>
-              <Form.Control
-                isInvalid={
-                  this.state.validation
-                    ? false
-                    : !this.checkElementValidation("zipCodeValidation")
-                }
-                placeholder="Kod pocztowy"
-                name="zip_code"
-                value={this.state.zip_code}
-                onChange={this.handleOnChange}
-              />
-              <Form.Control.Feedback type="invalid">
-                Wymagany format _ _ - _ _ _
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Form.Row>
-
-          <Button variant="secondary" onClick={this.handleSubmit}>
-            Zamawiam i płacę
-          </Button>
-        </Form>
-      </div>
-      : <EndOfOrder/>
-    );
-  }
-}
+        <Button variant="secondary" onClick={handleSubmit}>
+          Zamawiam i płacę
+        </Button>
+      </Form>
+    </div>
+  ) : (
+    <EndOfOrder />
+  );
+};
 
 const mapStateToProps = (state) => ({
   booksInCart: state.cart.booksInCart,
--- front/src/components/Navigation/Navigation.js
+++ front/src/components/Navigation/Navigation.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Navbar, Nav, Badge } from "react-bootstrap";
 import { connect } from "react-redux";
 import style from "../../App.module.css";
+import styles from "./Navigation.module.css";
 
 const Navigation = ({ totalBooks, children }) => {
   return (
@@ -10,16 +11,19 @@ const Navigation = ({ totalBooks, children }) => {
       <Navbar sticky="top" bg="dark" variant="dark">
         <Navbar.Brand>Księgarnia</Navbar.Brand>
         <Nav className="mr-auto">
-          <Link className={style.link} to="/">Strona główna</Link>
+          <Link className={style.link} to="/">
+            Strona główna
+          </Link>
         </Nav>
         <Nav>
           <Link to="/cart">
             {!!totalBooks && (
-              <Badge style={{ marginRight: "-0.7rem" }} variant="light">
+              <Badge style={{ marginRight: "-2.7rem" }} variant="light">
                 {totalBooks}
               </Badge>
             )}
             <img
+              className={styles.cartIcon}
               src="images/shopping-cart.svg"
               alt="cart"
               height="43"
