b07190e840598ea396179b9b22378af6e25f7315
Monika <monisiaz2103@gmail.com>
2021-04-11 22:21:12 +0200

    Changed BookCard design - cards for desktop view

--- src/pages/HomePage/BookCard.js
+++ src/pages/HomePage/BookCard.js
@@ -1,32 +1,39 @@
 import { connect } from "react-redux";
 import { addProductToCart } from "../../state/cart/actions";
 import Button from "../../components/Button";
-import "./homepage.scss"
+import "./homepage.scss";
 
-
-const BookCard= ({book, addProductToCart}) => {
-
-    const showPrice = (price) => {
-        return ((price / 100)).toFixed(2);
-      };
-    return (
-        <div className="card-container" key={book.id}>
-            <img className="card-info-icon" src="images/info-icon.svg"/>
-            <div className="image-container"> <img className="book-cover-image" src={book.cover_url}/></div>
-            <hr className="card-first-line"/>
-            <div>
-                <div className="card-small-container">
-                <p className="book-category">{book.category}</p>
-                <p className="book-title">{book.title}</p>
-                </div>
-                <p className="book-price">{showPrice(book.price)} zł</p>
-            </div>
-            <hr className="card-second-line"/>
-            <Button buttonName="Dodaj do koszyka" variant="primary" handleOnClick={() => addProductToCart(book.id)}/>
+const BookCard = ({ book, addProductToCart }) => {
+  const showPrice = (price) => {
+    return (price / 100).toFixed(2);
+  };
+  return (
+    <div className="card-container" key={book.id}>
+      <img className="card-info-icon" src="images/info-icon.svg" />
+      <div>
+        <div className="image-container">
+          {" "}
+          <img className="book-cover-image" src={book.cover_url} />
         </div>
-    )
-}
-
+        <hr className="card-first-line" />
+      </div>
+      <div className="card-small-container">
+        <p className="book-category">{book.category}</p>
+        <p className="book-title">{book.title}</p>
+        <p className="book-author">{book.author}</p>
+      </div>
+      <div>
+        <p className="book-price">{showPrice(book.price)} zł</p>
+        <hr className="card-second-line" />
+        <Button
+          buttonName="Dodaj do koszyka"
+          variant="primary"
+          handleOnClick={() => addProductToCart(book.id)}
+        />
+      </div>
+    </div>
+  );
+};
 
 const mapDispatchToProps = {
   addProductToCart,
